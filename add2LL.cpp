992b5160936b39cb5b794b30874229080b303da3
Abhi010bh <git21cs127-t@students.git.edu>
2024-04-07 15:11:33 +0530

    add2LL Updated

--- add2LL.cpp
+++ add2LL.cpp
@@ -1,4 +1,11 @@
 /**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -45,7 +52,62 @@ public:
             prev = newNode;
         }
 
-        // how do i maintain a head to the new linkedlist
+        
         return head;
     }
 };
+
+//approach 2:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int n1,n2,n3,carry=0;
+        ListNode* prevNode=NULL;
+        ListNode* head=NULL;
+        while(l1!=nullptr || l2!=nullptr){
+            
+            (l1==nullptr)?n1=0:n1=l1->val;
+            (l2==nullptr)?n2=0:n2=l2->val;
+            
+            n3=n1+n2+carry;
+            carry=0;
+            
+            if(n3>=10){
+                n3=n3-10;
+                carry=1;
+            }
+            ListNode* newNode=new ListNode(n3);
+            if(head==NULL){
+                head=newNode;
+            }
+            if(prevNode){
+                prevNode->next=newNode;
+            }
+            prevNode=newNode;
+            if(l1!=nullptr){
+                l1=l1->next;
+            }
+            if(l2!=nullptr){
+                l2=l2->next;
+            }
+        }
+        if(carry==1){
+            ListNode* newNode=new ListNode(carry);
+            prevNode->next=newNode;
+        }
+        
+        return head;
+    
+    }
+};
+
